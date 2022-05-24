4e468c434d5f757dabddfde6b45285e6d5ae78fb
aleks <aleksander.rebisz@gmail.com>
2022-05-24 16:22:17 +0200

    ver 1.0

--- redecorator.py
+++ redecorator.py
@@ -41,8 +41,12 @@ filePath = 'index.html'
 # check if the file exists
 if(os.path.isfile(filePath)):
 
-    # split filename into root and extension
+    # split filepath into root and extension
     splitTuple = os.path.splitext(filePath)
+
+    # extract the filename
+    filename = splitTuple[0]
+
     # extract the file extension
     fileExtension = splitTuple[1]
 
@@ -53,6 +57,9 @@ if(os.path.isfile(filePath)):
         textFile = open('index.html', 'r+')
         # read the file as a string
         htmlText = textFile.read()
+
+        #close the file
+        textFile.close()
         
         # check if the file contains a special expression at the beginning of the file
         if(htmlText.find('<!DOCTYPE html>') >= len(htmlText) - len(htmlText.lstrip())):
@@ -62,8 +69,6 @@ if(os.path.isfile(filePath)):
             # remove <!DOCTYPE html> from tag list
             tagList = tagList[1 : ]
             tagList = [tag[1 : -1] for tag in tagList]
-
-            print(tagList)
 
             if(checkTagMatching(tagList)):
                 # remove script tags only and style tags with content 
@@ -76,8 +81,6 @@ if(os.path.isfile(filePath)):
                             beginText = htmlText[ : htmlText.find('<' + openTag)]
                             endText = htmlText[htmlText.find(closeTag + '>') + len(closeTag) + 1 : ]
                             htmlText = beginText + endText
-            
-                print(tagList)
 
                 tagsToBeRemovedWithContent = ['figure', 'img', 'area', 
                                               'map', 'video' 'embed',
@@ -105,8 +108,6 @@ if(os.path.isfile(filePath)):
                     if('link' in tag.split() and 'rel="stylesheet"' in tag.split()): # reduce it to 'stylesheet' search
                         tagList.remove(tag)
                         htmlText = htmlText.replace('<' + tag + '>', '')
-            
-                print(tagList)
 
                 tagsToBeRemovedWithoutContent = ['big', 'strong', 'em', 
                                                  'i', 'kbd' 'mark',
@@ -120,18 +121,37 @@ if(os.path.isfile(filePath)):
                         htmlText = htmlText.replace('<' + openTag, '')
                         htmlText = htmlText.replace('</' + openTag + '>', '')
                 
-                print(tagList)
-                
                 for tag in tagList:
-                    styleAttribute = re.search(r'style(\s*=\s*"[^>\n]+")', tag)
-                    if(styleAttribute):
-                        newTag = tag.replace(styleAttribute.group(), '')
-                        tagList[tagList.index(tag)] = newTag
-                        htmlText = htmlText.replace(styleAttribute.group(), '')
-                
-                print(tagList)
+                    attributesWithValues = re.findall(r'[^\s\n]+\s*=\s*"[^>\n]+"', tag)
+                    for attribute in attributesWithValues:
+                        attributeName = attribute.split('=')[0]
+                        attributeValue = attribute.split('=')[1][1 : ].replace('"', '')
+                        if (attributeName == 'style'
+                        or   (attributeName == 'marginheight'
+                           or attributeName == 'marginwidth'
+                           and int(attributeValue) > 20)
+                        or   (attributeName == 'frameborder'
+                           or attributeName == 'border'
+                           and int(attributeValue) > 1)
+                        or   (attributeName == 'size'
+                           and int(attributeValue) > 40)
+                        or   (attributeName == 'height'
+                           or attributeName == 'width'
+                           and int(attributeValue) > 300)):
 
-                print(htmlText)
+                            newTag = tag.replace(attribute, '')
+                            tagList[tagList.index(tag)] = newTag
+                            htmlText = htmlText.replace(attribute, '')
 
-        #close the file
-        textFile.close()
+                # delete double spaces
+                while(htmlText.find('  ') != -1):
+                    htmlText = htmlText.replace('  ', '', 1)
+                # delete double new lines
+                while(htmlText.find('\n\n') != -1):
+                    htmlText = htmlText.replace('\n\n', '\n', 1)
+                # delete double new lines
+                while(htmlText.find('\n ') != -1):
+                    htmlText = htmlText.replace('\n ', '\n', 1)
+
+                with open(filename + '-sans-decor.html', 'w') as outfile:
+                    outfile.writelines(htmlText)
